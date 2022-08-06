08ac57b0d9fb18eb6f39fae82495b41d5453c796
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-08-06 08:32:02 +0530

    Create Check Whether A Year is A Leap Year Or Not.java

---
+++ Conditional Statements/Check Whether A Year is A Leap Year Or Not.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class YEAR {
+    public static void main(String[] args) {
+
+        Scanner z=new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter a Year :-");
+        a=z.nextInt();
+        if(a%4==0)
+        if(a%100==0)
+        if(a%400==0)
+        System.out.println("Leap Year");
+        else
+        System.out.println("Not Leap Year");
+        else
+        System.out.println("Leap Year");
+        else
+        System.out.println("Not Leap Year");
+
+    }
