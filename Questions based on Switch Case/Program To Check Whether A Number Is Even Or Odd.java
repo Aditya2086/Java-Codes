4cad94c8cd7f37d6f0d0c77b33e3b885674c3864
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-09-21 22:42:04 +0530

    Update and rename Write a C program to check whether a number is even or odd using a switch case.java to Program To Check Whether A Number Is Even Or Odd.java

--- Questions based on Switch Case/Program To Check Whether A Number Is Even Or Odd.java
+++ Questions based on Switch Case/Program To Check Whether A Number Is Even Or Odd.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
-class CheckOddEvenSwitch3{
-public static void main(String args[]){
-    Scanner scan=new Scanner(System.in); //create a scanner object for input
+
+class Main {
+    
+public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
+    
 System.out.print("Enter the integer number: ");
 
-int num=scan.nextInt();//get input from the user for num
+int num = sc.nextInt();
 
 switch(num%2){
 case 0:
@@ -14,6 +17,5 @@ case 1:
     System.out.println(num+" is an Odd number");
 
 }
-
 }
 }
