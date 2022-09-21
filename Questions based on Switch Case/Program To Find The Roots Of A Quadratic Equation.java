e03cc04bf90cfc6add375849306d4aeecd01b691
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-09-21 23:08:41 +0530

    Update and rename Write a C program to find the roots of a quadratic equation using a switch case.java to Program To Find The Roots Of A Quadratic Equation.java

---
+++ Questions based on Switch Case/Program To Find The Roots Of A Quadratic Equation.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Main {
+    
+    public static void main(String[] args) {
+        float a, b, c;
+        float r1,r2,i;
+        float d;
+        System.out.println("Enter values of a, b, c :");
+        Scanner sc = new Scanner(System.in);
+        
+        a = sc.nextFloat();
+        b = sc.nextFloat();
+        c = sc.nextFloat();
+        d = (b*b)-(4*a*c);
+        
+        switch(d>0)
+        {
+        case 1:
+
+        r1 = (-b+Math.sqrt(d))/(2*a);
+        r2 = (-b-Math.sqrt(d))/(2*a);
+        System.out.println("real roots:%.2f and %.2f"+ r1 + r2);
+        break;
+                
+        case 0:
+        switch(d<0)
+        {
+                
+        case 1:
+        r1=r2=-b/(2*a);
+        i = Math.sqrt(-d)/(2*a);
+        System.out.println("complex roots: %.2f + i%.2f and %.2f - i%.2f"+r1+i+r2+i);
+        break;
+                
+        case 0:
+        r1=r2=-b/(2*a);
+        System.out.println("Two equal and real roots: %.2f and %.2f"+r1+r2);
+        break;
+        }
+      }
+   }
+}
