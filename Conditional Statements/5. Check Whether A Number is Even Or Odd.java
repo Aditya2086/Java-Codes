72428776420f6cfa95e25dd3ae967a8645cf3518
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-10-06 18:40:13 +0530

    Update and rename Check Whether A Number is Even Or Odd.java to 5. Check Whether A Number is Even Or Odd.java

---
+++ Conditional Statements/5. Check Whether A Number is Even Or Odd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+int a,b;
+    
+System.out.println("Enter A Number :-");
+a=sc.nextInt();
+if(a%2==0)
+    
+System.out.println("Even Number");
+else
+System.out.println("Odd Number");
+
+}
+}
