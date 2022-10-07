42be622f2663df689d4a31d9f3cd8d6252c98455
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-10-07 11:37:29 +0530

    Update and rename 5. C Program to Find GCD of two Numbers.java to 5. Program To Find GCD Of Two Numbers.java

---
+++ Questions Based On Loops/5. Program To Find GCD Of Two Numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Main
+{  
+public static void main(String[] args)   
+{    
+int x = 12, y = 8, gcd = 1;  
+  
+for(int i = 1; i <= x && i <= y; i++)  
+{  
+if(x%i==0 && y%i==0)  
+gcd = i;  
+}  
+System.out.println("GCD Of %d and %d is :- %d", x, y, gcd);  
+}  
+}  
