09cbd5c7c630ac51f95666ed1fb47bcab97b1c80
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-10-07 11:56:40 +0530

    Update and rename 7. C Program to Display Characters from A to Z Using Loop.java to 7. Program To Display Characters From A To Z Using Loop.java

---
+++ Questions Based On Loops/7. Program To Display Characters From A To Z Using Loop.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main 
+{
+public static void main(String[] args) {
+char ch;
+
+System.out.println("Upper Case Alphabets Displayed \n");
+for(ch = 'A'; ch <= 'Z'; ++ch)
+	
+System.out.println(ch + " ");
+System.out.println("\n\n");
+
+System.out.println("Lower Case Alphabets Displayed \n");
+for(ch = 'a'; ch <= 'z'; ++ch)
+	
+System.out.println(ch + " ");
+}
+}
