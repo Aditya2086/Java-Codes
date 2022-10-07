d41cc5647ebbecf1e7debc2a76c7a9b8268c84c9
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-10-07 09:26:42 +0530

    Update and rename Program To Check Whether A Number Is Even Or Odd.java to 5. Program To Check Whether A Number Is Even Or Odd.java

--- Questions based on Switch Case/5. Program To Check Whether A Number Is Even Or Odd.java
+++ Questions based on Switch Case/5. Program To Check Whether A Number Is Even Or Odd.java
@@ -5,17 +5,17 @@ class Main {
 public static void main(String args[]) {
 Scanner sc = new Scanner(System.in);
     
-System.out.print("Enter the integer number: ");
+System.out.print("Enter The Integer Number :- ");
 
 int num = sc.nextInt();
 
 switch(num%2){
 case 0:
-    System.out.println(num+" is an Even number");
-  break;
+System.out.println(num+" is An Even Number");
+break;
+        
 case 1:
-    System.out.println(num+" is an Odd number");
-
+System.out.println(num+" is An Odd Number");
 }
 }
 }
